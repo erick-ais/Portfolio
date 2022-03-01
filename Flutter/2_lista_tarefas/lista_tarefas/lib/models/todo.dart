2dc5f06859a93471ff8c321a835b8986e5b1ba2b
Erick A. I. Souza <ais.projetostx@gmail.com>
2022-02-28 21:19:12 -0300

    Botão

---
+++ Flutter/2_lista_tarefas/lista_tarefas/lib/models/todo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Todo {
+  Todo({required this.title, required this.dateTime});
+
+  String title;
+  DateTime dateTime;
+}
--- Flutter/2_lista_tarefas/lista_tarefas/lib/widgets/todo_list_item.dart
+++ Flutter/2_lista_tarefas/lista_tarefas/lib/widgets/todo_list_item.dart
@@ -1,37 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
+import 'package:lista_tarefas/models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required this.title}) : super(key: key);
+  const TodoListItem({Key? key, required this.todo}) : super(key: key);
 
-  final String? title;
+  final Todo todo;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '16/11/2015',
-            style: TextStyle(
-              fontSize: 12,
+    return Slidable(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy').format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              ),
             ),
-          ),
-          Text(
-            title!,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      actionExtentRatio: 0.25,
+      actionPane: const SlidableDrawerActionPane(),
+      secondaryActions: [
+        IconSlideAction(
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
